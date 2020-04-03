02db3ecfb3320e105651b751334c758ae481d71b
Itchakhun <supadech.dw@gmail.com>
2020-04-03 18:18:14 +0700

    :zap: using fields

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const BLUE = 3447003;
 const RED = 15158332;
 const GREEN = 3066993;
 
+const addFeild = (inline = false) => name => value => ({ inline, name, value });
+
 module.exports.subscribeDiscord = function(event, context) {
   const build = eventToBuild(event.data);
   const status = ["WORKING", "SUCCESS", "FAILURE", "INTERNAL_ERROR", "TIMEOUT"];
@@ -18,14 +20,19 @@ module.exports.subscribeDiscord = function(event, context) {
   const commitUrl = `https://github.com/radiuszon/${repo}/commit/${commit}`;
   const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
 
+  const inlineField = addFeild(true);
+
   fetchGitMessage(commit).then(result => {
     axios.post(DISCORD_WEBHOOK_URL, {
       embeds: [
         {
-          title: `${repo}@${branch} is currently ${buildStatus}`,
-          description: getMessage(result),
           color: getColor(buildStatus),
-          url: commitUrl
+          fields: [
+            inlineField("Repo")(repo),
+            inlineField("Branch")(branch),
+            inlineField("Status")(buildStatus),
+            addFeild()("Commit")(getMessage(result))
+          ]
         }
       ]
     });
