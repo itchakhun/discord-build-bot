96e32bf6a1d57c56bec1add3280f5156ae3855c1
Itchakhun <16811182+itchakhun@users.noreply.github.com>
2020-07-04 23:38:10 +0700

    ♻️Added color, thumbnail, and url

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const axios = require("axios");
 
-const BLUE = 3447003;
-const RED = 15158332;
-const GREEN = 3066993;
+const GREY = 9545382;
+const BLUE = 39393;
+const RED = 16333359;
+const GREEN = 53606;
+const YELLOW = 16302848;
+
+const INFO_IMG = "https://img.icons8.com/color/2x/info.png";
+const ERROR_IMG = "https://img.icons8.com/flat_round/2x/stop.png";
+const WARNING_IMG = "https://img.icons8.com/flat_round/2x/pause.png";
+const SUCCESS_IMG = "https://img.icons8.com/flat_round/2x/checkmark.png";
 
 const addFeild = (inline = false) =>
   (name) => (value) => ({ inline, name, value });
 
 module.exports.subscribeDiscord = function (event, context) {
   const build = eventToBuild(event.data);
-  const status = ["WORKING", "SUCCESS", "FAILURE", "INTERNAL_ERROR", "TIMEOUT", "CANCELLED"];
+  const status = [
+    "WORKING",
+    "SUCCESS",
+    "FAILURE",
+    "INTERNAL_ERROR",
+    "TIMEOUT",
+    "CANCELLED",
+  ];
 
   if (!status.includes(build.status)) return;
 
   const buildStatus = build.status;
   const substitutions = build.substitutions;
+  const logUrl = build.logUrl;
   const branch = substitutions.BRANCH_NAME;
   const repo = substitutions.REPO_NAME;
   const commit = substitutions.COMMIT_SHA;
@@ -24,19 +39,26 @@ module.exports.subscribeDiscord = function (event, context) {
   const inlineField = addFeild(true);
 
   fetchGitMessage({ commit, repo, owner }).then((result) => {
+    const thumbnail = getThumbnail(buildStatus);
+    const color = getColor(buildStatus);
     axios.post(DISCORD_WEBHOOK_URL, {
       embeds: [
         {
-          color: getColor(buildStatus),
+          color,
+          thumbnail,
           fields: [
             inlineField("Repo")(repo),
             inlineField("Branch")(branch),
             inlineField("Status")(buildStatus),
+            addFeild()("See log")(logUrl),
             addFeild()("Commit")(getMessage(result)),
           ],
         },
       ],
-    });
+    })
+      .catch((e) => {
+        console.error(e.response.message);
+      });
   });
 };
 
@@ -44,8 +66,23 @@ const eventToBuild = (data) => {
   return JSON.parse(Buffer.from(data, "base64").toString());
 };
 
-const getColor =
-  (statusIndex) => ({ SUCCESS: GREEN, WORKING: BLUE }[statusIndex] || RED);
+const getColor = (
+  statusIndex,
+) => {
+  const colorSet = { SUCCESS: GREEN, WORKING: GREY, CANCELLED: YELLOW };
+  return colorSet[statusIndex] || RED;
+};
+
+const getThumbnail = (status) => {
+  const imgSet = {
+    SUCCESS: SUCCESS_IMG,
+    WORKING: INFO_IMG,
+    CANCELLED: WARNING_IMG,
+  };
+  return {
+    url: imgSet[status] || ERROR_IMG
+  };
+};
 
 const getMessage = (result) => {
   try {
